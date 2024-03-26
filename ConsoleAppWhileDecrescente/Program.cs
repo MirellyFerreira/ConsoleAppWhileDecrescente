4534253d0b0fff307f977332dbeaf86c38257c18
Usuario <Usuario@CPD02-M15>
2024-03-26 10:34:00 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppWhileDecrescente/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAppLaçoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 100;
+
+            while (cont >= 0)
+            {
+                Console.WriteLine(cont);
+                cont--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
